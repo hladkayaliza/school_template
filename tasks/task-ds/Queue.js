613b3b795fc572b7b973da508176cd8f2cfd2f6b
Liza <liza.hladkaya@gmail.com>
2021-03-02 22:01:41 +0300

    Add class fo DS

---
+++ tasks/task-ds/Queue.js
@@ -0,0 +1,46 @@
+class Queue {
+  constructor(maxLenght) {
+    this.maxLenght = maxLenght
+    this.state = []
+  }
+
+  enqueue(value) {
+    this.state.push(value)
+  }
+
+  dequeue() {
+    return this.state.unshift()
+  }
+
+  isEmpty() {
+    if (this.state?.length > 0) {
+      return false
+    }
+    return true
+  }
+
+  isFull() {
+    if (this.state?.length >= this.maxLenght) {
+      return true
+    }
+    return false
+  }
+
+  peek() {
+    return this.state[this.state.length - 1]
+  }
+}
+
+const queue1 = new Queue(3)
+queue1.enqueue(2)
+queue1.enqueue(7)
+queue1.enqueue(0)
+queue1.enqueue(9)
+console.log(queue1.isFull())
+queue1.dequeue()
+console.log(queue1.isFull())
+console.log(queue1.isEmpty())
+console.log(queue1.peek())
+
+
+
---
+++ tasks/task-ds/Stack.js
@@ -0,0 +1,51 @@
+class Stack {
+  constructor(maxLenght) {
+    this.maxLenght = maxLenght
+    this.state = []
+  }
+
+  push(value) {
+    this.state.push(value)
+  }
+
+  pop() {
+    this.state.pop()
+  }
+
+  isEmpty() {
+    if (this.state?.length > 0) {
+      return false
+    }
+    return true
+  }
+
+  peek() {
+    return this.state[-this.state.length + 1]
+  }
+
+  isFull() {
+    if (this.state?.length >= this.maxLenght) {
+      return true
+    }
+    return false
+  }
+}
+
+const stack1 = new Stack(5)
+
+stack1.push(8)
+stack1.push(3)
+stack1.push(4)
+stack1.push(9)
+stack1.pop()
+stack1.pop()
+stack1.push(12)
+stack1.push(16)
+stack1.push(13)
+stack1.push(22)
+console.log(stack1)
+
+console.log(stack1.isEmpty())
+console.log(stack1.isFull())
+console.log(stack1.peek())
+console.log(stack1)
