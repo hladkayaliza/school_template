ff8c84cefc50e35fceb12a2e93aea15bf8ce5451
Liza <liza.hladkaya@gmail.com>
2021-03-04 07:00:35 +0300

    Add style, change url for fetch

--- tasks/task-async/assets/scripts/index.js
+++ tasks/task-async/assets/scripts/index.js
@@ -1,6 +1,6 @@
 const FORM = document.getElementById('location-form')
-const CITY = document.getElementsByName('city')
-const COUNTRY = document.getElementsByName('country')
+const CITY = document.getElementById('city')
+const COUNTRY = document.getElementById('country')
 const APIKEY = 'e6dd5a990a4a4b7c5f138f291de09d4c'
 const URL = `http://api.weatherstack.com/current?access_key=${APIKEY}`
 const container = document.getElementsByClassName('weather-container')
@@ -20,36 +20,34 @@ const parseJson = function (result) {
   return result.json()
 }
 
-const getData = async function (city) {
-  const url = `${URL}&query=${city}`
+const getWeatherData = async function (city, country) {
+  const url = `${URL}&query=${city},${country}`
   const response = await fetch(url)
   return response
 }
 
-const render = function (data) {
-  debugger
-  console.log(data)
+const renderWeatherData = function (data) {
 
   const weatherNode = WEATHER_TEMPLATE.content.querySelector('.weather-item')
   const weather = weatherNode.cloneNode(true)
   weather.querySelector('.weather-img').src = data.current.weather_icons[0]
   weather.querySelector('.grad').textContent = `${data.current.temperature} °C`
-  weather.querySelector('.location').textContent = `${data.location.region}, ${data.location.country}`
-  weather.querySelector('.time').textContent = data.current.observation_time
-  weather.querySelector('.like').textContent = data.current.feelslike
+  weather.querySelector('.location').textContent = `${data.location.name}, ${data.location.country}`
+  weather.querySelector('.time').textContent = `Time: ${data.current.observation_time}`
+  weather.querySelector('.like').textContent = `Like: ${data.current.feelslike}`
   weather.querySelector('.state').textContent = data.current.weather_descriptions[0]
-  weather.querySelector('.wind').textContent = data.current.wind_dir
-  weather.querySelector('.speed').textContent = data.current.wind_speed
-  weather.querySelector('.pressure').textContent = data.current.pressure
+  weather.querySelector('.wind').textContent = `Wind: ${data.current.wind_dir}`
+  weather.querySelector('.speed').textContent = `Speed: ${data.current.wind_speed}`
+  weather.querySelector('.pressure').textContent = `Pressure: ${data.current.pressure}`
   container[0].append(weather)
 }
 
-async function GetWeather(city) {
+async function GetWeather(city, country) {
   try {
-    await getData(city)
+    await getWeatherData(city, country)
       .then((response) => checkStatus(response))
       .then((result) => parseJson(result))
-      .then((data) => render(data))
+      .then((data) => renderWeatherData(data))
   } catch (error) {
     console.log(error)
   }
@@ -60,10 +58,11 @@ FORM.addEventListener('submit', (evt) => {
   container[0].innerHTML = ''
   const cityValue = CITY.value
   const countryValue = COUNTRY.value
-  if ((!cityValue && !countryValue) || ) {
+  if (!cityValue) {
     container[0].innerHTML = '<span style="text-align: center">Please, input city</span>'
   } else {
-    GetWeather(cityValue)
+    GetWeather(cityValue, countryValue)
   }
-  DATA.value = ''
+  CITY.value = ''
+  COUNTRY.value = ''
 })
