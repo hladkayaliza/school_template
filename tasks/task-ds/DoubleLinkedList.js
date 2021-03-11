3ee0622e23c55a56b8c2957b73e41983d2a7b3d0
Liza <liza.hladkaya@gmail.com>
2021-03-11 18:44:18 +0300

    Fix error by iterating of array

--- tasks/task-ds/DoubleLinkedList.js
+++ tasks/task-ds/DoubleLinkedList.js
@@ -23,25 +23,24 @@ class DoubleLinkedList {
   traverse(order = true) {
     const array = []
     if (order) {
-      let currentNode = this.head()
+      let currentNode = this.head
       while (currentNode !== null) {
-        array.push(currentNode())
+        array.push(currentNode)
         currentNode = currentNode.next
       }
       return array
-    } else {
-      let currentNode = this.tail()
-      while (currentNode !== null) {
-        array.push(currentNode)
-        currentNode = currentNode.previous
-      }
-      return array
     }
+    let currentNode = this.tail
+    while (currentNode !== null) {
+      array.push(currentNode)
+      currentNode = currentNode.previous
+    }
+    return array
   }
 
   add(value) {
     const node = new Node(value)
-    if (!this.head()) {
+    if (!this.head) {
       this.head = node
       this.tail = node
     } else {
@@ -54,16 +53,15 @@ class DoubleLinkedList {
 
   getNode(value) {
     const targetNode = new Node(value)
-    if (!this.head()) {
+    if (!this.head) {
       return null
     }
-    let currentNode = this.head()
+    let currentNode = this.head
     while (currentNode.data !== targetNode.data) {
       if (!currentNode.next) {
         alert('Node is not exist in list')
         return null
       }
-      currentNode.previous = currentNode
       currentNode = currentNode.next
     }
     return currentNode
@@ -71,38 +69,37 @@ class DoubleLinkedList {
 
   addAfter(value, parentNode) {
     const newNode = new Node(value)
-    if (!this.head()) {
+    if (!this.head) {
       this.head = newNode
-      return this
-    } else {
-      let currentNode = this.head()
-      while (currentNode.data !== parentNode.data) {
-        if (!currentNode.next) {
-          alert('Target node is not exist in list')
-          return null
-        }
-        currentNode = currentNode.next
-        currentNode.previous = currentNode
-      }
-      currentNode.next = newNode
-      newNode.previous = currentNode
-      newNode.next = currentNode.next
+      this.tail = newNode
       return this
     }
+    let currentNode = this.head
+    while (currentNode.data !== parentNode.data) {
+      if (!currentNode.next) {
+        alert('Target node is not exist in list')
+        return null
+      }
+      currentNode = currentNode.next
+    }
+    newNode.previous = currentNode
+    newNode.next = currentNode.next
+    currentNode.next.previous = newNode
+    currentNode.next = newNode
+    return this
   }
 
   delete(value) {
-    if (!this.head()) {
+    if (!this.head) {
       alert('List is empty')
       return null
     }
     const targetNode = new Node(value)
-    let currentNode = this.head()
+    let currentNode = this.head
     while (currentNode.data !== targetNode.data) {
       if (!currentNode.next) {
         alert('Target node is not exist in list')
       }
-      currentNode.previous = currentNode
       currentNode = currentNode.next
     }
     const prevNode = currentNode.previous
@@ -113,40 +110,32 @@ class DoubleLinkedList {
   }
 
   isExist(value) {
-    if (!this.head()) {
+    if (!this.head) {
       alert('List is empty')
     }
     const targetNode = new Node(value)
-    let currentNode = this.head()
+    let currentNode = this.head
     while (currentNode.data !== targetNode.data) {
       if (!currentNode.next) {
-        alert('Target node is not exist in list')
         return false
-      } else {
-        currentNode.previous = currentNode
-        currentNode = currentNode.next
       }
+      currentNode = currentNode.next
     }
     return true
   }
 }
 
-const dll = new DoubleLinkedList();
+const dll = new DoubleLinkedList()
 
-dll.add('two').add('one').add('three').add('four');
+dll.add('two').add('one').add('three').add('four')
 dll.traverse() // two -> one -> three -> four
 dll.traverse(true) // two -> one -> three -> four
 dll.traverse(false) // four -> three -> one -> two
 
-dll.head() // Node with value === 'two'
-dll.tail() // Node with value === 'four'
-
 const parentNode = dll.getNode('one')
 dll.addAfter('ten', parentNode)
 dll.traverse() // two -> one -> ten -> three -> four
-
-dll.delete('one').delete('three');
+dll.delete('one').delete('three')
 dll.traverse() // two -> ten -> four
-
 dll.isExist('ten') // true
 dll.isExist('one') // false
